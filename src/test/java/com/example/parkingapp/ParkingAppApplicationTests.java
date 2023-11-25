231e0073fd9d1401ecaf9e0548e940b55f7c96f9
danX <danildryzov@gmail.com>
2023-11-25 13:19:06 +0100

    first commit

---
+++ src/test/java/com/example/parkingapp/ParkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
